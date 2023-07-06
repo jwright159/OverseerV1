68cffe30fed7edf5193a2efc682d7aacbaa2977d
Jade Wright <iamjwright159@gmail.com>
2023-07-06 12:47:21 -0400

    Theoretically switch back to mysqli until I can convert everything to query statements

--- includes/SQLconnect.php
+++ includes/SQLconnect.php
@@ -8,26 +8,33 @@ $db_username = $_ENV['DB_USERNAME'];
 $db_password = $_ENV['DB_PASSWORD'];
 $db_database = $_ENV['DB_DATABASE'];
 
+/*
 try {
 	$db = new PDO("mysql:host=$db_hostname;dbname=$db_database;", $db_username, $db_password);
 } catch (PDOException $e) {
 	exit("Could not connect to database: {$e->getMessage()}<br/>");
 }
+*/
+
+$mysqli = new \mysqli($db_hostname, $db_username, $db_password, $db_database);
+if (!$mysqli) {
+   die('Could not connect: ' . $mysqli->error);
+}
 
 function query(string $query, array|null $params = null)
 {
-	global $db;
-	$statement = $db->prepare($query);
+	global $mysqli;
+	$statement = $mysqli->prepare($query);
 	$statement->execute($params);
-	return $statement;
+	return $statement->get_result();
 }
 
 function fetchOne(string $query, array|null $params = null)
 {
-	return query($query, $params)->fetch();
+	return query($query, $params)->fetch_array();
 }
 
 function fetchAll(string $query, array|null $params = null)
 {
-	return query($query, $params)->fetchAll();
+	return query($query, $params)->fetch_all();
 }
